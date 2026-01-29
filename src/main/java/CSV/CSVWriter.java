0b508f3bb044abd657b4b344e940d8e455bb3306
victo <victor.e.ferron@gmail.com>
2026-01-29 10:59:37 -0500

    Completed CSVWriter

--- src/main/java/CSV/CSVWriter.java
+++ src/main/java/CSV/CSVWriter.java
@@ -14,6 +14,15 @@ public class CSVWriter {
     public void write(CSVWrapper csv) {
         for (List<String> csv_structure : csv.getCSVStructure()) {
 
+        try {
+            for (List<String> csv_element : csv.getCSVStructure()) {
+                writer.write(String.join(",", csv_element));
+                writer.newLine();
+                writer.close();
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println("Error: Could not write to CSV file. " + e.getMessage());
         }
     }
 }
