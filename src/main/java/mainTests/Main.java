46d210cd280244660846060802abb00d5cbca1c1
victo <victor.e.ferron@gmail.com>
2026-01-29 11:32:37 -0500

    fixed bug in Main class

--- src/main/java/mainTests/Main.java
+++ src/main/java/mainTests/Main.java
@@ -11,7 +11,27 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        File file = new File("C:\\Users\\victo\\test.csv");
+        File csv_file = new File("C:\\Users\\victo\\test.csv");
+
+        CSVWrapper csv;
+        try (BufferedReader br = new BufferedReader(new FileReader(csv_file))) {
+            CSVReader csvReader = new CSVReader(br);
+            csv = csvReader.read();
+        }
+
+        if (csv == null) {
+            System.out.println("csv was null");
+            return;
+        }
+
+        csv.delElement(1);
+        csv.addElement(List.of("Victor Emmanuel Ferron","victor.e.ferron@gmail.com"));
+        csv.addElement(List.of("Alice Maude Ferron","alic.m.ferron@gmail.com"));
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csv_file))) {
+            CSVWriter csvWriter = new CSVWriter(bw);
+            csvWriter.write(csv);
+        }
 
 
     }
