e1338a4f98b2139bf6278c1de6566998c7a8b938
victo <victor.e.ferron@gmail.com>
2026-01-29 07:57:33 -0500

    This is my CSV project

--- src/main/java/CSV/CSVReader.java
+++ src/main/java/CSV/CSVReader.java
@@ -1,9 +1,6 @@
 package CSV;
 
-import CSV.Exceptions.CSVFileNotFound;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +12,20 @@ public class CSVReader {
         this.reader = pReader;
     }
 
-    public CSVWrapper read(File file) throws IOException {
-        if (!file.exists()) {
-            throw new CSVFileNotFound("Could not find the CSV file");
-        }
+    public CSVWrapper read() throws IOException {
 
         String header_string = reader.readLine();
 
         CSVWrapper csv = new CSVWrapper(new ArrayList<String>(List.of(header_string.split(","))));
 
-        return null;
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isBlank()) {
+                continue;
+            }
+            csv.addElement(new ArrayList<>(List.of(line.split(","))));
+        }
+
+        return csv;
     }
 }
--- src/main/java/CSV/Exceptions/CSVFileNotFound.java
+++
@@ -1,7 +0,0 @@
-package CSV.Exceptions;
-
-public class CSVFileNotFound extends RuntimeException {
-    public CSVFileNotFound(String message) {
-        super(message);
-    }
-}
