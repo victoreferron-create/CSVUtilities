77aaf02b644a77243f611b63db818e8b9dceb9e7
victo <victor.e.ferron@gmail.com>
2026-01-29 07:22:23 -0500

    This is my CSV project

--- src/main/java/mainTests/Main.java
+++ src/main/java/mainTests/Main.java
@@ -7,17 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> csv_header = new ArrayList<>();
 
-        csv_header.addFirst("name");
-        csv_header.add("age");
-        CSVWrapper csv = new CSVWrapper(csv_header);
-
-
-
-
-        csv.addElement(List.of("Victor", Integer.toString(12)) , 1);
-
-        System.out.println(csv.getElement(1));
     }
 }
