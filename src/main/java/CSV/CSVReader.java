2d684d61834195270f663ba90080ec46bf3bd3ab
victo <victor.e.ferron@gmail.com>
2026-01-29 08:45:26 -0500

    this is my CSV project

--- src/main/java/CSV/CSVReader.java
+++ src/main/java/CSV/CSVReader.java
@@ -30,7 +30,6 @@ public class CSVReader {
         }
 
         String line;
-        // this is a comment from GitHub!!!
         while ((line = reader.readLine()) != null) {
             if (line.isBlank()) {
                 continue;
