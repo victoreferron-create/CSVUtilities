d629bade4fc70d42d86ff0b46ec4b2e680134bfa
victo <victor.e.ferron@gmail.com>
2026-01-29 08:25:01 -0500

    Next version will include a CSVWriter

--- src/main/java/mainTests/Main.java
+++ src/main/java/mainTests/Main.java
@@ -1,12 +1,25 @@
 package mainTests;
 
+import CSV.CSVReader;
 import CSV.CSVWrapper;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        File file = new File("C:\\Users\\victo\\OneDrive\\Desktop\\test.csv");
+
+        CSVReader csvreader = new CSVReader(new BufferedReader(new FileReader(file)));
+
+        CSVWrapper csv = csvreader.read();
+
+        System.out.println(csv.getElement(1));
+
+        csv.modifyElement(1, 1, "13");
+
+        System.out.println(csv.getElement(1));
     }
 }
