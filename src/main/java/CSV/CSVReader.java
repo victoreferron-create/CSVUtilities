8318f24a8d4547a2e6e1e8fa4681412a4d4ff155
victo <victor.e.ferron@gmail.com>
2026-01-29 07:25:21 -0500

    This is my CSV project

--- src/main/java/CSV/CSVReader.java
+++ src/main/java/CSV/CSVReader.java
@@ -1,4 +1,11 @@
 package CSV;
 
+import java.io.BufferedReader;
+
 public class CSVReader {
+    BufferedReader reader;
+
+    public CSVReader(BufferedReader pReader) {
+        this.reader = pReader;
+    }
 }
